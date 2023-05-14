beef4c1002f6d949ce1869bcf9550561e03849d8
diegohp <diego.blake11@gmail.com>
2023-05-13 19:51:11 -0600

    [ADD] copy button and animations

--- src/main.js
+++ src/main.js
@@ -2,14 +2,19 @@
 const encrypter = new Encrypter();
 
 //HTML elements
+
 const textarea = document.getElementById('message');
 const warning = document.querySelector('.encrypter__note');
+//buttons
 const encryptButton = document.getElementById('encrypt');
 const decryptButton = document.getElementById('decrypt');
-const emptyResult = document.querySelector('.result__none');
-const result = document.querySelector('.result__encrypted');
-const encResult = document.getElementById('encrypter-result');
+const copyButton = document.getElementById('copy-button');
+//divs
+const result = document.querySelector('.result');
+//classname
 const warningClass = 'wrong-text';
+
+//UI functions
 
 function addWarning() {
   warning.classList.add(warningClass);
@@ -19,25 +24,65 @@ function removeWarning() {
   warning.classList.remove(warningClass);
   textarea.classList.remove(warningClass);
 }
-
 //higlihts in red the textarea and text when the message doesn't fit in the requirements
 textarea.addEventListener('input', () => {
   encrypter.message = textarea.value;
   !encrypter.validateData() ? addWarning() : removeWarning();
 });
 
-//function for the encryptButton
+function showResult(msg) {
+  result.innerHTML = `
+  <div class="result__encrypted">
+    <p>
+      ${msg}
+    </p>
+    <button onclick="copy()">Copiar</button>
+  </div>
+  `;
+}
+
+function showEmpty() {
+  result.innerHTML = `
+  <div class="result__none">
+    <picture>
+      <source
+        media="(min-width:1000px)"
+        srcset="./assets/icons/searching-bro.svg"
+      />
+      <img src="" alt="" />
+    </picture>
+    <h3>Ningún mensaje fue encontrado</h3>
+    <p>Ingresa el texto que desees encriptar o desencriptar.</p>
+  </div>`;
+}
+
+function showCopyMessage() {
+  const resultDiv = document.querySelector('.result__encrypted');
+  const msg = document.createElement('span');
+  msg.innerText = '¡Copiado a la papelera!';
+  resultDiv.append(msg);
+}
+
+function copy() {
+  navigator.clipboard
+    .writeText(encrypter.encrypted)
+    .then(showCopyMessage, () => {
+      console.log('FAILED');
+    });
+}
+
+//Encrypter functions
+
+//encrypts/decrypts and shows the result if the message is valid.
 function execute(fn) {
   if (encrypter.message.length < 1 || !encrypter.valid) {
-    emptyResult.style.display = 'block';
-    result.style.display = 'none';
+    showEmpty();
   } else {
-    emptyResult.style.display = 'none';
-    result.style.display = 'flex';
-    //adds the encrypted/decrypted message to the HTML element
-    encResult.innerText = fn();
+    const msg = fn();
+    showResult(msg);
   }
 }
+
 //function for the encryptButton
 encryptButton.addEventListener('click', () =>
   execute(encrypter.encrypt.bind(encrypter))
